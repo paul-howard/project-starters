8ffec8b1f174dc48f7880d3cc90c2a5c114b3965
Paul Howard <phoward8020+github@gmail.com>
2015-06-08 10:25:55 -0700

    Configure view engine and NODE_ENV settings

--- express/express-basic/app/controllers/index.server.controller.js
+++ express/express-basic/app/controllers/index.server.controller.js
@@ -1,3 +1,5 @@
 exports.render = function(req, res) {
-  res.send('Everything appears to be working. (Edit app-name/app/controllers/index.server.controller.js to get rid of me.)');
+  res.render('index', {
+    title: 'Hello World'
+  });
 };
--- express/express-basic/config/express.js
+++ express/express-basic/config/express.js
@@ -1,7 +1,30 @@
-var express = require('express');
+var express    = require('express');
+var morgan     = require('morgan');
+var compress   = require('compression');
+var bodyParser = require('body-parser');
 
 module.exports = function() {
   var app = express();
+
+  // Define NODE_ENV-dependant middlewares
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  } else if (process.env.NODE_ENV === 'production') {
+    app.use(compress());
+  }
+
+  // Define non-dependant middlewares
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+
+  app.set('views', './app/views');
+  app.set('view engine', 'ejs');
+
+  // Require all routes files
   require('../app/routes/index.server.routes.js')(app);
+
+  // Return configured app
   return app;
 };
